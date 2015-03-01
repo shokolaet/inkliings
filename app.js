5111326a3716b1c69b41782a0c5cafc993c0777a
Krystal Xu <xx5xm@virginia.edu>
2015-02-28 21:20:41 -0500

    Instagram working, LinkedIn not

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var path = require('path');
-var instagram = require('instagram-node').instagram();
-var Twitter = require('twitter');
 var env = require('./env');
 //var favicon = require('serve-favicon');
 
@@ -9,6 +7,10 @@ var app = express();
 
 // load environmental variables
 env.loadEnvVariables();
+
+var instagram = require('instagram-node').instagram();
+var Twitter = require('twitter');
+//var Linkedin = require('node-linkedin')(process.env['LINKEDIN_KEY'], process.env['LINKEDIN_SECRET'], 'linkedinCallback');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,28 +20,69 @@ app.set('view engine', 'html');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use('/static', express.static(__dirname + '/static'));
 
-instagram.use({ client_id: process.env['INSTAGRAM_CLIENT_ID'],
-    client_secret: process.env['INSTAGRAM_CLIENT_SECRET'] });
-
-app.get("/linkedin", function (req, res) {
-    var data = {
-        LINKEDIN_KEY: process.env['LINKEDIN_KEY']
-    };
-    res.render('index.ejs', data);
+instagram.use({
+    client_id: process.env['INSTAGRAM_CLIENT_ID'],
+    client_secret: process.env['INSTAGRAM_CLIENT_SECRET']
 });
 
 app.get("/instagram", function (req, res) {
-    instagram.user_search('classickristie', function(err, users, remaining, limit) {
-        //console.log(users);
-        res.type('html');
-        var data = {
-            LINKEDIN_KEY: process.env['LINKEDIN_KEY'],
-            user: users[0]
-        };
-        res.render('index.ejs', data);
-    });
+    var data = {
+        LINKEDIN_KEY: process.env['LINKEDIN_KEY'],
+        instagramUserError: false,
+        instagramMediaError: false
+    };
+    // TODO: change username
+    instagram.user_search('taylorswift', function (err, users, remaining, limit) {
+        if (err || users.length == 0) {
+            data['instagramUserError'] = true;
+        } else {
+            data['instagramUser'] = users[0];
+        }
+        //Test people:
+        //kristie: 16303615 (private user)
+        //swift: 11830955 (public user)
 
+        // TODO: change id
+        instagram.user_media_recent('11830955', function (err, medias, pagination, remaining, limit) {
+            if (err) {
+                data['instagramMediaError'] = err;
+            } else {
+                if (medias.length > 5) { // get 5 photos max
+                    data['instagramNumPhotos'] = 5;
+                } else {
+                    data['instagramNumPhotos'] = medias.length;
+                }
+                data['instagramPhotos'] = medias.slice(0, data['instagramNumPhotos']);
+            }
+
+            res.type('html');
+            res.render('index.ejs', data);
+        });
+    });
 });
+
+//var linkedinCallback = function () {
+//    app.get('/oauth/linkedin', function(req, res) {
+//        // This will ask for permisssions etc and redirect to callback url.
+//        Linkedin.auth.authorize(res, ['r_basicprofile']);
+//        return res.redirect('http://localhost:1337/oauth/linkedin/callback');
+//    });
+//
+//    app.get('/oauth/linkedin/callback', function(req, res) {
+//        Linkedin.auth.getAccessToken(res, req.query.code, function(err, results) {
+//            if ( err )
+//                return console.error(err);
+//
+//            /**
+//             * Results have something like:
+//             * {"expires_in":5184000,"access_token":". . . ."}
+//             */
+//
+//            console.log(results);
+//            return res.redirect('http://localhost:1337/instagram');
+//        });
+//    });
+//};
 
 var twitterClient = new Twitter({
     consumer_key: process.env['TWITTER_CONSUMER_KEY'],
@@ -51,7 +94,7 @@ var twitterClient = new Twitter({
 var params = {screen_name: 'shokolaet'};
 
 app.get("/twitter", function (req, res) {
-    twitterClient.get('statuses/user_timeline', params, function(error, tweets, response){
+    twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
             console.log(tweets);
         } else {
--- static/linkedin.js
+++ static/linkedin.js
@@ -1,14 +1,23 @@
 var linkedinOnLoad = function () {
     if (!IN.User.isAuthorized()) {
-        IN.User.authorize(authorizeCallback);
-    } else {
-        console.log("Already logged in");
-        IN.API.Raw("/people/~?format=json").method("GET").body().result(function (person) {
-            console.log("Welcome " + person.firstName + " " + person.lastName);
+        IN.User.authorize(function () {
+            console.log('Logging in to LinkedIn');
+            loggedInCallback();
         });
+    } else {
+        console.log("Already logged in to LinkedIn");
+        loggedInCallback();
     }
 };
 
-var authorizeCallback = function() {
-    console.log('Logged in');
+var loggedInCallback = function () {
+    console.log("Logged in to LinkedIn");
+    console.log(IN.User);
+    // TODO: replace ID dynamically
+    // TODO: DOESN'T WORK!
+    IN.API.Raw("/people-search?keywords=orange%20shake&sort=connections?format=json").method("GET").body().result(function (person) {
+        console.log(person);
+        $('#linkedin-name').text(person.firstName + " " + person.lastName);
+        $('#linkedin-headline').text(person.headline);
+    });
 };
