9362e8cd6a1356852797846e1dd5ccf985ff8d4c
ke <kc5ez@virginia.edu>
2015-03-01 09:02:17 -0500

    front end

--- app.js
+++ app.js
@@ -61,6 +61,40 @@ app.get("/instagram", function (req, res) {
     });
 });
 
+app.get("/insta", function (req, res) {
+    var data = {
+        LINKEDIN_KEY: process.env['LINKEDIN_KEY'],
+        instagramUserError: false,
+        instagramMediaError: false
+    };
+    // TODO: change username
+    instagram.user_search('taylorswift', function (err, users, remaining, limit) {
+        if (err || users.length == 0) {
+            data['instagramUserError'] = true;
+        } else {
+            data['instagramUser'] = users[0];
+        }
+        //Test people:
+        //kristie: 16303615 (private user)
+        //swift: 11830955 (public user)
+
+        // TODO: change id
+        instagram.user_media_recent('11830955', function (err, medias, pagination, remaining, limit) {
+            if (err) {
+                data['instagramMediaError'] = err;
+            } else {
+                if (medias.length > 5) { // get 5 photos max
+                    data['instagramNumPhotos'] = 5;
+                } else {
+                    data['instagramNumPhotos'] = medias.length;
+                }
+                data['instagramPhotos'] = medias.slice(0, data['instagramNumPhotos']);
+            }
+            res.send(data);
+        });
+    });
+});
+
 //var linkedinCallback = function () {
 //    app.get('/oauth/linkedin', function(req, res) {
 //        // This will ask for permisssions etc and redirect to callback url.
@@ -91,16 +125,39 @@ var twitterClient = new Twitter({
     access_token_secret: ''
 });
 
-var params = {screen_name: 'shokolaet'};
+var params = {screen_name: 'shokolaet', count: 5};
 
 app.get("/twitter", function (req, res) {
     twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
-            console.log(tweets);
+            //console.log(tweets);
+			res.type('html');
+			var data = {tweets: tweets};
+			res.render('home.ejs', data);
         } else {
             console.log(error);
         }
     });
+});
+
+app.get("/tweet", function (req, res) {
+    twitterClient.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+			var data = {tweets: tweets};
+			res.send(data);
+        } else {
+            console.log(error);
+        }
+    });
+});
+
+
+app.get("/", function (req, res) {
+   res.render('home.ejs');
+});
+
+app.get("/about", function (req, res) {
+   res.render('about.ejs');
 });
 
 module.exports = app;
